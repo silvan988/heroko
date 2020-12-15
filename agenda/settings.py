159af23bbbf493e0bea45537fcbaa13dbf6cbd57
Silvan Matsuo <silvan754@gmail.com>
2020-12-14 21:11:44 -0300

    troca de db

--- agenda/settings.py
+++ agenda/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'contatos.apps.ContatosConfig'
+    'contatos.apps.ContatosConfig',
 ]
 
 MIDDLEWARE = [
